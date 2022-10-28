e8cf07e454736e0ae67682a0f156301523793c36
github_username <kasprzak.malg@gmail.com>
2022-10-29 00:29:17 +0200

    making cards "disabled"

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -49,8 +50,9 @@ function App() {
   //compare two selected cards
 
   useEffect(() => {
-    if (choiceOne && choiceTwo) {
 
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -66,7 +68,7 @@ function App() {
         resetTurn()
 
       } else {
-        setTimeout(() => resetTurn(), 500)
+        setTimeout(() => resetTurn(), 1000)
       }
 
     }
@@ -84,6 +86,7 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     // setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
 
 
@@ -99,6 +102,7 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
 
 
           />
